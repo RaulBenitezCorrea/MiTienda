5d1b0c5a4c8a99768796f3d7898dd3200c532979
RaulBenitezCorrea <rauly2402@gmail.com>
2025-04-09 14:36:06 -0600

    aaa

--- view/home.php
+++ view/home.php
@@ -9,16 +9,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
-    <p>Esta es la vista de inicio.</p>
-
-    <!-- Botones para navegar a otras vistas -->
-    <a href="<?php echo BASE_URL; ?>usuarios/listar">
-        <button>Ir a Usuarios</button>
-    </a>
-    <a href="<?php echo BASE_URL; ?>productos/ver">
-        <button>Ir a Productos</button>
-    </a>
+aaaaa
 
     <script src="https://kit.fontawesome.com/33f535655a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
